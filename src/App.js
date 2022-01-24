a7a8a3413dadf769e5b967daf820c019c2c32565
makkut <maksim.kutprot@gmail.com>
2022-01-24 13:00:49 +0100

    new3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
 import Welcome from './components/Welcome/Welcome';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
 import NotDefined from './components/NotDefined/NotDefined';
 
 function App() {
     return (
         <div className='conatainer z-depth-3'>
-            <HashRouter>
+            <Router basename='/todolist-react-firebase'>
                 <Routes>
                     <Route path='/' element={<Welcome />} />
                     <Route path='/homepage' element={<HomePage />} />
                     <Route path='*' element={<NotDefined />} />
                 </Routes>
-            </HashRouter>
+            </Router>
         </div>
     );
 }
