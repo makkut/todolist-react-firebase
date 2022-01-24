2fa76ed9a0c42f37532ef204cf2b57e85876ba1f
makkut <maksim.kutprot@gmail.com>
2022-01-24 14:36:06 +0100

    first commit4

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
 import Welcome from './components/Welcome/Welcome';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
 import NotDefined from './components/NotDefined/NotDefined';
 
 function App() {
     return (
         <div className='conatainer z-depth-3'>
-            <HashRouter basename={process.env.PUBLIC_URL}>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <Routes>
                     <Route path='/' element={<Welcome />} />
                     <Route path='/homepage' element={<HomePage />} />
                     <Route path='*' element={<NotDefined />} />
                 </Routes>
-            </HashRouter>
+            </BrowserRouter>
         </div>
     );
 }
